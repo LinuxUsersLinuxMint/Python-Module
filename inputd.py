57ae4b8992d05ba176ab193d1ae9023fc91666b4
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-06 23:23:03 +0300

    Add files via upload

---
+++ inputd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Sayi Değişkenleri / Number inputs
+
+sayi1=input('1. sayiyi giriniz: ')
+sayi2=input('2. sayiyi giriniz: ')
+sayi3=input('3. sayiyi giriniz: ')
+sayi4=input('4. sayiyi giriniz: ')
+sayi5=input('5. sayiyi giriniz: ')
+sayi6=input('6. sayiyi giriniz: ')
+sayi7=input('7. sayiyi giriniz: ')
+sayi8=input('8. sayiyi giriniz: ')
+sayi9=input('9. sayiyi giriniz: ')
+sayi10=input('10. sayiyi giriniz: ')
