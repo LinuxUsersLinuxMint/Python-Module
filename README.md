6095e57dd359295c7a608f671dedd990972e65d3
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-07 16:29:06 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 
 Nasıl Kullanılır: herhangi bir python dosyanıza girip Kodun İlk satırına "from inputd import *" ekleyin
 NOT: inputd modulunun çalışma ortamının ve klasörünün içinde bulunması gerekir.
+NOT2: inputd modulunu kullanmadan önce kendinize göre özelleştirmenizi ve kodlarda değişiklik yapmanızı öneririz.
+
+* Hangi değişkenleri içerdiğini görmek için inputd.py a veya input.txt dosyasına bakabilirsiniz.
 
 * Python inputd module.
 "Pre-creates variables created for Python"
@@ -13,3 +16,6 @@ NOT: inputd modulunun çalışma ortamının ve klasörünün içinde bulunması
 
 How to Use: go into any of your python files and add "from inputd import *" to First line of Code
 NOTE: The inputd module must be located inside the runtime and its folder.
+NOTE2: Before using the inputd module, we recommend that you customize it and make changes to the codes.
+
+* You can look at inputd.py or input.txt to see what variables it contains.
--- inputd.py
+++ inputd.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"Copyright© 2023 LinuxUsersLinuxMint"
+"Python-Module Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır."
+"Python-Module All Rights Reserved under the GPL(General Public License)."
+"Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
+"A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 
 # Sayi Değişkenleri / Number inputs
 
@@ -23,3 +28,21 @@ number7=input('Enter the Seventh Number: ')
 number8=input('Enter the Eighth Number: ')
 number9=input('Enter the Ninth Number: ')
 number10=input('Enter the tenth number: ')
+
+# Hesap Makinesi Değişkenleri / Calcutator inputs
+
+top=float(sayi1)+float(sayi2)  # Not Bu sadece bir değişkendir ve sayi1 ve sayi2 yi kullanır isterseniz bunu değiştirebilirsiniz.
+cık=float(sayi1)-float(sayi2)
+carp=float(sayi1)*float(sayi2)
+bol=float(sayi1)/float(sayi2)
+yuzde=float(sayi1)%float(sayi2)
+
+islem=input('Gerçekleştirmek İstediğiniz İşlemi Giriniz: ')
+
+collect=float(number1)+float(number2)
+Extraction=float(number1)-float(number2)
+Impact=float(number1)*float(number2)
+Divide=float(number1)/float(number2)
+Percentage=float(number1)%float(number2)
+
+process=input('Enter the action you want to perform: ')
