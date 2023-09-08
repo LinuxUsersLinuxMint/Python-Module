6b3a5450cc8ed4308cdd43bed2623e4e49452b2e
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-08 22:40:28 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 from inputd import *
 
-sayi_1()
+if islem=="top":
+    top()
+elif islem=="cık":
+    cık()
+elif islem=="carp":
+    carp()
+elif islem=="bol":
+    bol()
+elif islem=="yuzde":
+    yuzde()
+else:
+    print("Invalid Process...")
