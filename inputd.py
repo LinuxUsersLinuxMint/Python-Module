7e12326ca33d19a8cfd1b3d9c2d211cac8ebd1dc
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-08 22:33:26 +0300

    Add files via upload

--- inputd.py
+++ inputd.py
@@ -1,49 +1,76 @@
 #!/usr/bin/python3
-"Copyright© 2023 LinuxUsersLinuxMint"
-"Python-Module Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır."
-"Python-Module All Rights Reserved under the GPL(General Public License)."
-"Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
-"A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
-
-# Sayi Değişkenleri / Number inputs
-
-sayi1=input('1. sayiyi giriniz: ')
-sayi2=input('2. sayiyi giriniz: ')
-sayi3=input('3. sayiyi giriniz: ')
-sayi4=input('4. sayiyi giriniz: ')
-sayi5=input('5. sayiyi giriniz: ')
-sayi6=input('6. sayiyi giriniz: ')
-sayi7=input('7. sayiyi giriniz: ')
-sayi8=input('8. sayiyi giriniz: ')
-sayi9=input('9. sayiyi giriniz: ')
-sayi10=input('10. sayiyi giriniz: ')
-
-number1=input('Enter First Number: ')
-number2=input('Enter Second Number: ')
-number3=input('Enter the Third Number')
-number4=input('Enter the Fourth Number: ')
-number5=input('Enter the Fifth Number: ')
-number6=input('Enter the Sixth Number: ')
-number7=input('Enter the Seventh Number: ')
-number8=input('Enter the Eighth Number: ')
-number9=input('Enter the Ninth Number: ')
-number10=input('Enter the tenth number: ')
+# Copyright© 2023 LinuxUsersLinuxMint
+# Python-Module Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
+# Python-Module All Rights Reserved under the GPL(General Public License).
+# Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
+# A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
 
 # Hesap Makinesi Değişkenleri / Calcutator inputs
 
-top=float(sayi1)+float(sayi2)  # Not Bu sadece bir değişkendir ve sayi1 ve sayi2 yi kullanır isterseniz bunu değiştirebilirsiniz.
-cık=float(sayi1)-float(sayi2)
-carp=float(sayi1)*float(sayi2)
-bol=float(sayi1)/float(sayi2)
-yuzde=float(sayi1)%float(sayi2)
+def top():
+    sayi1=int(input('1.sayiyi giriniz: '))
+    sayi2=int(input('2.sayiyi giriniz: '))
+    sonuc = sayi1 + sayi2
+    print("{0} + {1} = {2}". format(sayi1,sayi2,sonuc))
+    return
+def cık():
+    sayi1=int(input('1.sayiyi giriniz: '))
+    sayi2=int(input('2.sayiyi giriniz: '))
+    sonuc = sayi1 - sayi2
+    print("{0} + {1} = {2}". format(sayi1,sayi2,sonuc))
+    return
+def carp():
+    sayi1=int(input('1.sayiyi giriniz: '))
+    sayi2=int(input('2.sayiyi giriniz: '))
+    sonuc = sayi1 * sayi2
+    print("{0} + {1} = {2}". format(sayi1,sayi2,sonuc))
+    return
+def bol():
+    sayi1=int(input('1.sayiyi giriniz: '))
+    sayi2=int(input('2.sayiyi giriniz: '))
+    sonuc = sayi1 / sayi2
+    print("{0} + {1} = {2}". format(sayi1,sayi2,sonuc))
+    return
+def yuzde():
+    sayi1=int(input('1.sayiyi giriniz: '))
+    sayi2=int(input('2.sayiyi giriniz: '))
+    sonuc = sayi1 % sayi2
+    print("{0} + {1} = {2}". format(sayi1,sayi2,sonuc))
+    return
 
 islem=input('Gerçekleştirmek İstediğiniz İşlemi Giriniz: ')
 
-addition=float(number1)+float(number2)
-subtraction=float(number1)-float(number2)
-multiplication=float(number1)*float(number2)
-division=float(number1)/float(number2)
-percentage=float(number1)%float(number2)
+def addition():
+    number1=int(input('Enter the First Number: '))
+    number2=int(input('Enter the Second Number: '))
+    result = number1 + number2
+    print("{0} + {1} = {2}". format(number1,number2,result))
+    return
+def subraction():
+    number1=int(input('Enter the First Number: '))
+    number2=int(input('Enter the Second Number: '))
+    result = number1 - number2
+    print("{0} + {1} = {2}". format(number1,number2,result))
+    return
+def multiplication():
+    number1=int(input('Enter the First Number: '))
+    number2=int(input('Enter the Second Number: '))
+    result = number1 * number2
+    print("{0} + {1} = {2}". format(number1,number2,result))
+    return
+def division():
+    number1=int(input('Enter the First Number: '))
+    number2=int(input('Enter the Second Number: '))
+    result = number1 / number2
+    print("{0} + {1} = {2}". format(number1,number2,result))
+    return
+def Percentage():
+    number1=int(input('Enter the First Number: '))
+    number2=int(input('Enter the Second Number: '))
+    result = number1 % number2
+    print("{0} + {1} = {2}". format(number1,number2,result))
+    return
+
 
 process=input('Enter the action you want to perform: ')
 
---
+++ test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from inputd import *
+
+sayi_1()
